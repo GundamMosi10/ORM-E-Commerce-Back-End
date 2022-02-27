29affe2eb2be0266feba81e062164441d8e809df
Colin Mosi Cargile <cargile10@gmail.com>
2022-02-27 18:27:52 -0500

    added the code for the ProductTag.js to define the columns now moving on the index.js in the models folder

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -4,9 +4,28 @@ const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
 
-ProductTag.init(
+ProductTag.init(// define columns
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER, 
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    prouct_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Tag',
+        key: 'id',
+      }
+    }
   },
   {
     sequelize,
