288b0b21b1a1690e936e14165f6540454483a486
Colin Mosi Cargile <cargile10@gmail.com>
2022-02-28 17:55:11 -0500

    finished the code for the get one product api in the product route file

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -26,6 +26,25 @@ router.get('/', (req, res) => {
 
 // get one product
 router.get('/:id', (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{
+        model: Category, 
+        attributes: ['category_name']
+      }, 
+    {
+      model: Tag, 
+      attributes: ['tag_name']
+    }]
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No product has been found with this id!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
