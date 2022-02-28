df7173623514ac2d0075125b1f337bc64c6a0cd9
Colin Mosi Cargile <cargile10@gmail.com>
2022-02-27 19:03:53 -0500

    added the code for the index.js in the models folder now going to move onto working on the routes folder

--- models/index.js
+++ models/index.js
@@ -4,17 +4,23 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-Product.belongsTo(Category, {
-  through: {
-    model: Tag, 
-    unique: false
-  },
+
+Product.belongsTo(Category, {  // Products belongsTo Category
+  foreignKey: 'Category_id'
 });
-// Categories have many Products
 
-// Products belongToMany Tags (through ProductTag)
+Category.hasMany(Product, {  // Categories have many Products
+  foerignKey: 'Category_id'
+});
 
+Product.belongsToMany(tag, { // Products belongToMany Tags (through ProductTag)
+  through: ProductTag,
+  foerignKey: 'Product_id'
+}),
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+}),
 
 module.exports = {Product, Category, Tag, ProductTag };
