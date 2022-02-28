1e73e7591560c667fb95bc77d75f65d73a824cda
Colin Mosi Cargile <cargile10@gmail.com>
2022-02-28 17:50:47 -0500

    adding the code to get all the products in the api/products and working on the get one product code in the same file

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,6 +5,21 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      include: [{
+        model: Category, 
+        attributes: ['id', 'category_name']
+      },
+    {
+      model: Tag, 
+      attributes: ['id', 'tag_name']
+    }]
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find all products
   // be sure to include its associated Category and Tag data
 });
