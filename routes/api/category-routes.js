145ab0d7bd8bc0cf1894aead472fea3f2a321467
Colin Mosi Cargile <cargile10@gmail.com>
2022-02-28 18:25:10 -0500

    adding code for the category routes for get all and get by PK

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,11 +4,37 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: {
+        model: Product, 
+        attributes: ['product_name']
+      },
+    });
+    res.status(200).json(categoryData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
   // find all categories
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: {
+        model: Product, 
+        attributes: ['category_id']
+      },
+    });
+    if (!categoryData) {
+      res.status(404).json({message: 'There is no category for this id!'});
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find one category by its `id` value
   // be sure to include its associated Products
 });
