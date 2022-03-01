b17dd4757757742a830c88165e557a4c20f32520
Colin Mosi Cargile <cargile10@gmail.com>
2022-02-28 19:10:36 -0500

    started working on the tag routes in the apis folder

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,16 +4,37 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
+  try {
+
+  }
   // find all tags
   // be sure to include its associated Product data
 });
 
 router.get('/:id', (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id);
+    if(!tagData) {
+      res.status(404).json({ message: 'No tag with this id!'});
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
 router.post('/', (req, res) => {
+  try {
+    const tagData = await Tag.create({
+      tag_name: req.body.tag_name
+    });
+    res.status(200).json(tagData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
   // create a new tag
 });
 
