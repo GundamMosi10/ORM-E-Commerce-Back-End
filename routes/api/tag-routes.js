3fd16dd11af0c7a33baed21efdcd197d6483e2cd
Colin Mosi Cargile <cargile10@gmail.com>
2022-02-28 19:32:51 -0500

    added the code for the delete and update in the tag routes working on the endpoint api for tags to get all of them

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -39,10 +39,41 @@ router.post('/', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
+  try {
+    const tagData = await Tag.update(req.body, {
+      tag_name: req.body.tag_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }),
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag with this id!'});
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch(err) { 
+    res.status(500).json(err);
+  }
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if(!tagData) {
+      res.status(404).json({message: 'No tag with this id!'});
+      return;
+    }
+    res.status(200).json(tagData); 
+  } catch(err) {
+    res.status(500).json(err);
+  }
   // delete on tag by its `id` value
 });
 
